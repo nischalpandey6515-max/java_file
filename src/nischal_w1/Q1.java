c1250fa4b7d54e4ad1b545ad1505b2efc961e16a
nischal pandey <nischalpandey6515@gmail.com>
2026-03-09 23:41:08 +0545

    initial commit

--- src/nischal_w1/Q1.java
+++ src/nischal_w1/Q1.java
@@ -1,14 +1,12 @@
 package nischal_w1;
 
 public class Q1 {
-public static void main(String[] args) {
- String name="nischal pandey";
- int age = 18;
- int student_number=2616991;
- System.out.println("My name is  "+name);
- System.out.println("Age :"+age);
- System.out.println("studnet id :"+student_number);
- 
-}
-
+	public static void main (String[] args) {
+		String name = "Nischal";
+		int age = 18;
+		int id = 2616991;
+		System.out.println("My name is :"+name);
+		System.out.println("Age is : "+age);
+		System.out.println("id  "+id);
+	}
 }
